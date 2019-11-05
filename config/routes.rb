41ee4d17cff12ff639b084466fae04d0809c3581
Hugo Valla <Hugo_valla@hotmail.fr>
2019-11-05 15:30:14 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # READ
+   get "tasks", to: "tasks#index"
+   get "tasks/new", to: "tasks#new", as: :new
+   post "tasks", to: "tasks#create"
+   get "tasks/:id", to: "tasks#show", as: :task
+
+  # CREATE
+
+
+   # CREATE FORM
+
+
+  #UPDATE GET THE FORM
+   get "tasks/:id/edit", to: "tasks#edit", as: :edit
+
+   # UPDATE PATCH THE FORM
+   patch "tasks/:id", to: "tasks#update"
+
+   # DELETE
+   delete "tasks/:id", to: "tasks#destroy"
+
+   #ROOT
+    root to: 'tasks#index'
+
 end
